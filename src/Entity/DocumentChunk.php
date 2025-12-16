9362210efaf27b6c550bfb3951b5e8613b3c517b
Space Wolf <wolfmanIII@gmail.com>
2025-12-17 00:08:43 +0100

    push

--- src/Entity/DocumentChunk.php
+++ src/Entity/DocumentChunk.php
@@ -7,6 +7,9 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: DocumentChunkRepository::class)]
+#[ORM\Table(name: 'document_chunk', indexes: [
+    new ORM\Index(name: 'document_chunk_embedding_hnsw', columns: ['embedding']),
+])]
 class DocumentChunk
 {
     #[ORM\Id]
